e86388e592c79c6432404461d5dbb83951ba9ab1
Janethra Dissanayake <janethradissanayake@gmail.com>
2026-02-24 17:21:44 +0530

    Added few testing lines

--- tests/Functional/Module 1/home.spec.js
+++ tests/Functional/Module 1/home.spec.js
@@ -35,10 +35,33 @@ test.describe('Module 1 - Home', () => {
     await expect(page.getByRole('heading', { name: 'WELCOME TO OFFICE ONE' })).toBeVisible();
   });
 
-  test.only('Verify Home button redirects to the homepage', async ({ page }) => {
+  test('Verify Home button redirects to the homepage', async ({ page }) => {
     await page.goto('https://www.officeone.lk/');
     await page.locator('#menu-item-1093').click();
     await expect(page).toHaveURL('https://www.officeone.lk/');
+  });
+
+  test('Verify Office One Kandy button redirects to the Kandy site', async ({ page }) => {
+    await page.goto('https://www.officeone.lk/');
+    await page.locator('#menu-item-2917').click();
+    await expect(page).toHaveURL('https://www.officeone.lk/office-one-kandy/');
+  });
+
+  test('Verify Office One Colombo button redirects to the Colombo site', async ({ page }) => {
+    await page.goto('https://www.officeone.lk/');
+    await page.locator('#menu-item-2916').click();
+    await expect(page).toHaveURL('https://www.officeone.lk/office-one-colombo/');
+  });
+
+  test('Verify banner video visibility', async ({ page }) => {
+    await page.goto('https://www.officeone.lk/');
+    await expect(page.locator('#post-1183 div').filter({ hasText: 'WELCOME TOOFFICE ONE Flexible' }).nth(2)).toBeVisible();
+  });
+
+  test.only('Verify kandy site navigation', async ({ page }) => {
+    await page.goto('https://www.officeone.lk/');
+    await page.locator('#menu-item-2917').getByRole('link', { name: 'Office One Kandy' }).click();
+    await page.goto('https://www.officeone.lk/office-one-kandy/');
   });
 
 });
